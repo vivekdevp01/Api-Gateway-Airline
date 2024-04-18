4e679ff1600bbab44239bdfdcb7a0fb5eb293d37
Vivek Singh Pundir <23f100036@ds.study.iitm.ac.in>
2024-04-18 14:11:14 +0530

    Added rate limiter in my project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 
 const { ServerConfig } = require('./config');
+const rateLimit=require('express-rate-limit')
 const apiRoutes = require('./routes');
 
 const app = express();
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 15 minutes
+	max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	
+})
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+app.use(limiter)
+
 
 app.use('/api', apiRoutes);
 
